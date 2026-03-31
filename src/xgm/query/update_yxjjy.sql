ee2ebd1528b6c6a700cc3180549728538584459d
huangtao-hz <huangtao.sh@icloud.com>
2026-03-31 12:07:39 +0800

    upgrade

--- src/xgm/query/update_yxjjy.sql
+++ src/xgm/query/update_yxjjy.sql
@@ -1,8 +1,7 @@
 -- 新增生产上已下架的交易
 insert into xjjy
-select a.jym,b.jym,b.tcrq,"1-直接下架",null
+select a.jym,"1-直接下架",null
 from xmjh a
-left join xjdz b on a.jym = b.yjym
 left join jym c on a.jym=trim(c.jym)
 left join xjjy d on a.jym=d.jym
 where c.jym is null and d.jym is null
